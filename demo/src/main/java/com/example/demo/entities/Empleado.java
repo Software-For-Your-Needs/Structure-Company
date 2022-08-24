a9f521f0c512a2ad66c25420549bcde45abb09fa
carivantelva <carlostello_25@hotmail.com>
2022-08-24 17:19:26 -0500

    se agrega clse empleado

---
+++ demo/src/main/java/com/example/demo/entities/Empleado.java
@@ -0,0 +1,63 @@
+package com.example.demo.entities;
+
+public class Empleado {
+    private String nombreEmpleado;
+    private String correoEmpleado;
+    private String rollEmpleado;
+    private int idEmpleado;
+    private Empresa empresaEmpleado;
+
+    // metodo constructor
+
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombreEmpleado, String correoEmpleado, String rollEmpleado, int idEmpleado, Empresa empresaEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+        this.correoEmpleado = correoEmpleado;
+        this.rollEmpleado = rollEmpleado;
+        this.idEmpleado = idEmpleado;
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getRollEmpleado() {
+        return rollEmpleado;
+    }
+
+    public void setRollEmpleado(String rollEmpleado) {
+        this.rollEmpleado = rollEmpleado;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Empresa getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(Empresa empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+}
